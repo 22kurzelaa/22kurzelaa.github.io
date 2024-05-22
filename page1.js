910d2eaefb881e0257384f9e6bb163b1d51eb06c
22kurzelaa <22kurzelaa@wellswayschool.com>
2024-05-22 13:20:56 +0100

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,12 +1,1 @@
-<html>
-<head>
-<title>Website</title>
-</head>
-<body>
-<h1>Welcome</h1>
-<p>
-<a href="page2.html" target="_self">Link to page 2</a>
-</p>
-</body>
-
-</html>
+<button onclick="location.href = 'https://22kurzelaa-github-io.vercel.app/lessons';" id="myButton" class="float-left submit-button" >Home</button>
